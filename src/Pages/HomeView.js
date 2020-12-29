f52107fed98c6691859b645416ec856bbb531011
Avix <dev.avinash.yogan@gmail.com>
2020-12-29 21:28:57 +0100

    change css

--- src/Pages/HomeView.js
+++ src/Pages/HomeView.js
@@ -9,8 +9,8 @@ class HomeView extends Component {
 
     render() {
         return(
-            <div className="" style={{marginTop:'10%',float: 'left', width: '100%', paddingLeft:'10%'}} id="repositories">
-            <div style={{textAlign: 'left',float: 'left'}}><p style={{fontSize: '1.8rem'}}>ECE Organization</p>
+            <div className="" style={{marginTop:'5%',float: 'left', width: '100%', paddingLeft:'40%'}} id="repositories">
+            <div style={{textAlign: 'left'}}><p style={{fontSize: '1.8rem'}}>ECE Organization</p>
 
                 <ul>
                     {this.props.gitlab_ece.map(function(name, index){
@@ -18,7 +18,7 @@ class HomeView extends Component {
                     })}
                 </ul>
             </div>
-        <div style={{textAlign: 'right',float: 'left'}}><p style={{fontSize: '1.8rem'}}>Public</p>
+        <div style={{textAlign: 'left',marginTop:'10%'}}><p style={{fontSize: '1.8rem'}}>Public</p>
             <ul>
                 {this.props.gitlab_public.map(function(name, index){
                     return <div key={ index }>{name}</div>;
