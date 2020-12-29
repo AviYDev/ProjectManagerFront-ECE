2ffe42171e0c39e22b76957a941a12195e70bf31
Avix <dev.avinash.yogan@gmail.com>
2020-12-29 21:34:57 +0100

    change css

--- src/Pages/HomeView.js
+++ src/Pages/HomeView.js
@@ -9,7 +9,7 @@ class HomeView extends Component {
 
     render() {
         return(
-            <div className="" style={{marginTop:'5%',float: 'left', width: '100%', paddingLeft:'40%'}} id="repositories">
+            <div className="" style={{marginTop:'5%',float: 'left', width: '100%', paddingLeft:'20%'}} id="repositories">
             <div style={{textAlign: 'left'}}><p style={{fontSize: '1.8rem'}}>ECE Organization</p>
 
                 <ul>
